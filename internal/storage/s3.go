6e0c33d407a882ea383630b15a8fab208ca3754e
Michael Bang <mic@vbang.dk>
2023-12-22 15:11:41 +0100

    storage: add S3Storage

---
+++ internal/storage/s3.go
@@ -0,0 +1,199 @@
+package storage
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/micvbang/go-helpy/filey"
+	"github.com/micvbang/simple-commit-log/internal/infrastructure/logger"
+)
+
+type S3Storage struct {
+	log            logger.Logger
+	s3             s3iface.S3API
+	topicCacheRoot string
+	bucketName     string
+}
+
+type S3StorageInput struct {
+	S3             s3iface.S3API
+	LocalCacheRoot string
+	BucketName     string
+	RootDir        string
+	Topic          string
+}
+
+func NewS3Storage(log logger.Logger, input S3StorageInput) (*Storage, error) {
+	s3Storage := &S3Storage{
+		log:            log,
+		s3:             input.S3,
+		bucketName:     input.BucketName,
+		topicCacheRoot: input.LocalCacheRoot,
+	}
+
+	return NewStorage(log, s3Storage, input.RootDir, input.Topic)
+}
+
+func (ss *S3Storage) Writer(recordBatchPath string) (io.WriteCloser, error) {
+	cacheRecordBatchPath := ss.recordBatchCachePath(recordBatchPath)
+	log := ss.log.
+		WithField("cacheRecordBatchPath", cacheRecordBatchPath).
+		WithField("recordBatchPath", recordBatchPath)
+
+	log.Debugf("checking cache for record batch")
+	if filey.Exists(cacheRecordBatchPath) {
+		log.Debugf("record already exists")
+		return nil, fmt.Errorf("file already exists '%s'", cacheRecordBatchPath)
+	}
+
+	log.Debugf("creating cache file")
+	f, err := ss.createCacheFile(cacheRecordBatchPath)
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("creating s3WriteCloser")
+
+	writeCloser := &s3WriteCloser{f: f, s3Upload: func(rd io.ReadSeeker) error {
+		_, err := ss.s3.PutObject(&s3.PutObjectInput{
+			Bucket: &ss.bucketName,
+			Key:    &recordBatchPath,
+			Body:   rd,
+		})
+		return err
+	}}
+
+	return writeCloser, nil
+}
+
+func (ss *S3Storage) Reader(recordBatchPath string) (io.ReadSeekCloser, error) {
+	cacheRecordBatchPath := ss.recordBatchCachePath(recordBatchPath)
+	log := ss.log.
+		WithField("cacheRecordBatchPath", cacheRecordBatchPath).
+		WithField("recordBatchPath", recordBatchPath)
+
+	log.Debugf("checking cache for record batch")
+
+	// check if file is already cached
+	f, err := os.Open(cacheRecordBatchPath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, fmt.Errorf("checking for file in cache '%s': %w", cacheRecordBatchPath, err)
+	}
+	if f != nil {
+		// file in cache, don't fetch from s3
+		return f, nil
+	}
+
+	log.Debugf("fetching record batch from s3")
+	// file not in cache
+	obj, err := ss.s3.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(ss.bucketName),
+		Key:    &recordBatchPath,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("retrieving s3 object: %w", err)
+	}
+	defer obj.Body.Close()
+
+	log.Debugf("creating cache file")
+	f, err = ss.createCacheFile(cacheRecordBatchPath)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Debugf("copying s3 object to cache file")
+	_, err = io.Copy(f, obj.Body)
+	if err != nil {
+		return nil, fmt.Errorf("writing s3 object to disk '%s': %w", cacheRecordBatchPath, err)
+	}
+
+	return f, nil
+}
+
+func (ss *S3Storage) ListFiles(topicPath string, extension string) ([]string, error) {
+	log := ss.log.
+		WithField("topicPath", topicPath).
+		WithField("extension", extension)
+
+	fileNames := make([]string, 0, 128)
+
+	topicPath, _ = strings.CutPrefix(topicPath, "/")
+
+	log.Debugf("listing objects in s3")
+	err := ss.s3.ListObjectsPages(&s3.ListObjectsInput{
+		Bucket: aws.String(ss.bucketName),
+		Prefix: &topicPath,
+	}, func(objects *s3.ListObjectsOutput, b bool) bool {
+		for _, obj := range objects.Contents {
+			if obj == nil || obj.Key == nil {
+				continue
+			}
+
+			filePath := *obj.Key
+
+			if strings.HasSuffix(filePath, extension) {
+				fileNames = append(fileNames, *obj.Key)
+			}
+		}
+		return true
+	})
+
+	log.Debugf("found %d files", len(fileNames))
+
+	return fileNames, err
+}
+
+func (ss *S3Storage) recordBatchCachePath(recordBatchPath string) string {
+	return filepath.Join(ss.topicCacheRoot, recordBatchPath)
+}
+
+func (ss *S3Storage) createCacheFile(cacheRecordBatchPath string) (*os.File, error) {
+	log := ss.log.WithField("cacheRecordBatchPath", cacheRecordBatchPath)
+
+	log.Debugf("creating cache dirs")
+	err := os.MkdirAll(filepath.Dir(cacheRecordBatchPath), os.ModePerm)
+	if err != nil {
+		return nil, fmt.Errorf("creating cache topic dir: %w", err)
+	}
+
+	log.Debugf("creating cache file")
+	f, err := os.Create(cacheRecordBatchPath)
+	if err != nil {
+		return nil, fmt.Errorf("creating cache record batch '%s': %w", cacheRecordBatchPath, err)
+	}
+
+	return f, err
+}
+
+type s3WriteCloser struct {
+	f        *os.File
+	s3Upload func(io.ReadSeeker) error
+}
+
+func (swc *s3WriteCloser) Write(b []byte) (int, error) {
+	return swc.f.Write(b)
+}
+
+func (swc *s3WriteCloser) Close() error {
+	_, err := swc.f.Seek(0, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("seeking to beginning: %w", err)
+	}
+
+	err = swc.s3Upload(swc.f)
+	if err != nil {
+		return fmt.Errorf("uploading to s3: %w", err)
+	}
+
+	err = swc.f.Close()
+	if err != nil {
+		return fmt.Errorf("closing file: %w", err)
+	}
+
+	return nil
+}
---
+++ internal/storage/s3_test.go
@@ -0,0 +1,133 @@
+package storage
+
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/micvbang/go-helpy/stringy"
+	"github.com/micvbang/simple-commit-log/internal/infrastructure/logger"
+	"github.com/stretchr/testify/require"
+)
+
+var log = logger.NewDefault(context.Background())
+
+// TestS3WriteToS3 verifies that Writer creates an io.WriteCloser that calls
+// s3's PutObject method with the given data once the io.WriteCloser is closed.
+func TestS3WriteToS3(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "scl_*")
+	require.NoError(t, err)
+
+	bucketName := "mybucket"
+	recordBatchPath := "topicName/000123.record_batch"
+	recordBatchBody := []byte(stringy.RandomN(500))
+
+	s3Mock := &S3Mock{}
+	s3Mock.MockPutObject = func(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+		// Verify the expected parameters are passed on to S3
+		require.Equal(t, *input.Bucket, bucketName)
+		require.Equal(t, *input.Key, recordBatchPath)
+
+		gotBody, err := io.ReadAll(input.Body)
+		require.NoError(t, err)
+		require.Equal(t, recordBatchBody, gotBody)
+
+		return nil, nil
+	}
+
+	s3Storage := &S3Storage{
+		log:            log,
+		s3:             s3Mock,
+		topicCacheRoot: tempDir,
+		bucketName:     bucketName,
+	}
+
+	// Test
+	rbWriter, err := s3Storage.Writer(recordBatchPath)
+	require.NoError(t, err)
+
+	n, err := rbWriter.Write(recordBatchBody)
+	require.NoError(t, err)
+	require.Equal(t, len(recordBatchBody), n)
+
+	// Verify
+	// file should not be written to s3 before it's closed
+	require.False(t, s3Mock.PutObjectCalled)
+
+	// file should be written to s3 when it's closed
+	err = rbWriter.Close()
+	require.NoError(t, err)
+	require.True(t, s3Mock.PutObjectCalled)
+}
+
+// TestS3WriteToCache verifies that Writer creates an io.WriteCloser that writes
+// the data written to it to a file in the given cache directory.
+func TestS3WriteToCache(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "scl_*")
+	require.NoError(t, err)
+
+	recordBatchPath := "topicName/000123.record_batch"
+	recordBatchBody := []byte(stringy.RandomN(500))
+
+	s3Mock := &S3Mock{}
+	s3Mock.MockPutObject = func(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+		return nil, nil
+	}
+
+	s3Storage := &S3Storage{
+		log:            log,
+		s3:             s3Mock,
+		topicCacheRoot: tempDir,
+		bucketName:     "mybucket",
+	}
+
+	// Test
+	rbWriter, err := s3Storage.Writer(recordBatchPath)
+	require.NoError(t, err)
+
+	n, err := rbWriter.Write(recordBatchBody)
+	require.NoError(t, err)
+	require.Equal(t, len(recordBatchBody), n)
+
+	err = rbWriter.Close()
+	require.NoError(t, err)
+
+	// Verify
+	rd, err := s3Storage.Reader(recordBatchPath)
+	require.NoError(t, err)
+
+	cacheBody, err := io.ReadAll(rd)
+	require.NoError(t, err)
+	require.Equal(t, recordBatchBody, cacheBody)
+}
+
+type S3Mock struct {
+	s3iface.S3API
+
+	MockPutObject   func(*s3.PutObjectInput) (*s3.PutObjectOutput, error)
+	PutObjectCalled bool
+
+	MockGetObject   func(*s3.GetObjectInput) (*s3.GetObjectOutput, error)
+	GetObjectCalled bool
+
+	MockListObjectPages   func(*s3.ListObjectsInput, func(*s3.ListObjectsOutput, bool) bool) error
+	ListObjectPagesCalled bool
+}
+
+func (sm *S3Mock) PutObject(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+	sm.PutObjectCalled = true
+	return sm.MockPutObject(input)
+}
+
+func (sm *S3Mock) GetObject(input *s3.GetObjectInput) (*s3.GetObjectOutput, error) {
+	sm.GetObjectCalled = true
+	return sm.MockGetObject(input)
+}
+
+func (sm *S3Mock) ListObjectsPages(input *s3.ListObjectsInput, f func(*s3.ListObjectsOutput, bool) bool) error {
+	sm.ListObjectPagesCalled = true
+	return sm.MockListObjectPages(input, f)
+}
