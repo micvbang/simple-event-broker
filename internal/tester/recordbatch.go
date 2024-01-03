6f11673dbdc01b5937c8ac93c1488894a96ac73b
Michael Bang <mic@vbang.dk>
2024-01-03 20:36:10 +0100

    tester/MakeRandomRecordBatch: randomize record size

--- internal/tester/recordbatch.go
+++ internal/tester/recordbatch.go
@@ -1,11 +1,14 @@
 package tester
 
-import "github.com/micvbang/go-helpy/stringy"
+import (
+	"github.com/micvbang/go-helpy/inty"
+	"github.com/micvbang/go-helpy/stringy"
+)
 
 func MakeRandomRecordBatch(size int) [][]byte {
 	expectedRecordBatch := make([][]byte, size)
 	for i := 0; i < len(expectedRecordBatch); i++ {
-		expectedRecordBatch[i] = []byte(stringy.RandomN(10))
+		expectedRecordBatch[i] = []byte(stringy.RandomN(1 + inty.RandomN(50)))
 	}
 	return expectedRecordBatch
 }
