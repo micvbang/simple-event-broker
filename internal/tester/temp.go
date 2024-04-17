3692b2121b449d815167e348fef3c4d13a9373ec
Michael Bang <mic@vbang.dk>
2024-04-17 14:32:19 +0200

    tester: add TempFile()

--- internal/tester/temp.go
+++ internal/tester/temp.go
@@ -14,3 +14,11 @@ func TempDir(t *testing.T) string {
 
 	return tempDir
 }
+
+// TempFile calls os.CreateTemp and fails the test if there is an error.
+func TempFile(t *testing.T) *os.File {
+	f, err := os.CreateTemp("", "seb_*")
+	require.NoError(t, err)
+
+	return f
+}
