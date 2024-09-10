d0e3cd56e97e43d68d9df74bc47424a4572cb176
Michael Bang <mic@vbang.dk>
2024-09-10 11:47:45 +0200

    app/benchmark-read: restructure and improve outputs

--- cmd/seb/app/benchmark_read.go
+++ cmd/seb/app/benchmark_read.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/micvbang/go-helpy"
@@ -25,6 +26,7 @@ func init() {
 
 	fs.IntVar(&benchmarkReadFlags.logLevel, "log-level", int(logger.LevelWarn), "Number of times to run the benchmark")
 	fs.IntVarP(&benchmarkReadFlags.numRuns, "runs", "r", 1, "Number of times to run the benchmark")
+	fs.StringVar(&benchmarkReadFlags.topicName, "topic-name", "some-topic", "Name of topic to use for test")
 	fs.IntVarP(&benchmarkReadFlags.numWorkers, "workers", "w", runtime.NumCPU()*2, "Number of workers to add data")
 	fs.BoolVarP(&benchmarkReadFlags.localBroker, "local-broker", "l", true, "Whether to start a broker only for these benchmarks")
 	fs.StringVar(&benchmarkReadFlags.remoteBrokerAddress, "remote-broker-address", "http://localhost:51313", "Address of remote broker to connect to instead of starting local broker")
@@ -33,12 +35,11 @@ func init() {
 	fs.IntVar(&benchmarkReadFlags.numBatches, "batches", 4096, "Number of records to generate")
 	fs.IntVar(&benchmarkReadFlags.recordSize, "record-size", 1024, "Size of records in bytes")
 	fs.IntVar(&benchmarkReadFlags.numRecordsPerBatch, "records-per-batch", 1024, "Records per batch")
-	fs.IntVar(&benchmarkReadFlags.requestRecords, "request-records", 1024, "Records per request")
+	fs.IntVar(&benchmarkReadFlags.numRequests, "requests", 10_000, "Number of requests to send")
+	fs.IntVar(&benchmarkReadFlags.numRecordsPerRequest, "records-per-request", 1024, "Records per request")
 
 	fs.IntVar(&benchmarkReadFlags.batchBytesMax, "batcher-max-bytes", 50*sizey.MB, "Maximum number of bytes to wait before committing incoming batch to storage")
 	fs.DurationVar(&benchmarkReadFlags.batchBlockTime, "batcher-block-time", 5*time.Millisecond, "Maximum amount of time to wait before committing incoming batches to storage")
-
-	fs.DurationVarP(&benchmarkReadFlags.benchmarkDuration, "duration", "d", 30*time.Second, "Amount of time to run the benchmark for")
 }
 
 var benchmarkReadCmd = &cobra.Command{
@@ -49,16 +50,27 @@ var benchmarkReadCmd = &cobra.Command{
 		flags := benchmarkReadFlags
 		log := logger.NewWithLevel(context.Background(), logger.LogLevel(flags.logLevel))
 
-		numRecords := flags.numBatches * flags.numRecordsPerBatch
 		bytesPerBatch := flags.numRecordsPerBatch * flags.recordSize
-		totalBytes := numRecords * flags.recordSize
-		fmt.Printf("Generating %d batches of size %s (%d records), totalling %s\n", flags.numBatches, sizey.FormatBytes(bytesPerBatch), numRecords, sizey.FormatBytes(totalBytes))
+		fmt.Printf("Benchmark config:\n")
+		fmt.Printf("Num workers:\t\t%d\n", flags.numWorkers)
+		fmt.Printf("Num requests:\t\t%d\n", flags.numRequests)
+		fmt.Printf("Records/request:\t%d\n", flags.numRecordsPerRequest)
+
+		if flags.localBroker {
+			totalBytesRequested := flags.numRequests * flags.numRecordsPerRequest * flags.recordSize
+			bytesPerRequest := flags.numRecordsPerRequest * flags.recordSize
+			fmt.Printf("Record size:\t\t%v (%vB)\n", sizey.FormatBytes(flags.recordSize), flags.recordSize)
+			fmt.Printf("Bytes/request:\t\t%s (%dB)\n", sizey.FormatBytes(bytesPerRequest), bytesPerRequest)
+			fmt.Printf("Total bytes:\t\t%s (%dB)\n", sizey.FormatBytes(totalBytesRequested), totalBytesRequested)
+
+			fmt.Printf("\nLocal broker config:\n")
+			fmt.Printf("Batch block time:\t%s\n", flags.batchBlockTime)
+			fmt.Printf("Batch bytes max:\t%s (%d)\n", sizey.FormatBytes(flags.batchBytesMax), flags.batchBytesMax)
+		}
+		fmt.Printf("\n")
 
 		batchPool := syncy.NewPool(func() *sebrecords.Batch {
-			return helpy.Pointer(sebrecords.NewBatch(make([]uint32, 0, flags.requestRecords), make([]byte, 0, 50*sizey.MB)))
-		})
-		bufPool := syncy.NewPool(func() []byte {
-			return make([]byte, 0, 50*sizey.MB)
+			return helpy.Pointer(sebrecords.NewBatch(make([]uint32, 0, flags.numRecordsPerRequest), make([]byte, 0, 50*sizey.MB)))
 		})
 
 		var broker sebbench.Broker
@@ -73,67 +85,57 @@ var benchmarkReadCmd = &cobra.Command{
 			}
 		}
 
-		const topicName = "some-topic"
 		client, err := broker.Start()
 		if err != nil {
 			log.Fatalf("broker start: %w", err)
 		}
 
-		// TODO: make it configurable whether to insert data or not
-		// insert data to read
 		if flags.localBroker {
-			fmt.Printf("Inserting %d batches of size %s (%d records), totalling %s to prepare for read benchmark\n", flags.numBatches, sizey.FormatBytes(bytesPerBatch), numRecords, sizey.FormatBytes(totalBytes))
+			numRecords := flags.numBatches * flags.numRecordsPerBatch
+			bytesToInsert := numRecords * flags.recordSize
+			fmt.Printf("Inserting %d batches of size %s (%d records), totalling %s to prepare for read benchmark\n",
+				flags.numBatches, sizey.FormatBytes(bytesPerBatch), numRecords, sizey.FormatBytes(bytesToInsert))
 
 			batches := make(chan sebrecords.Batch, 8)
 			go sebbench.GenerateBatches(log, batches, flags.numBatches, flags.numRecordsPerBatch, flags.recordSize)
 
 			wg := &sync.WaitGroup{}
 			t0 := time.Now()
-			httpAddRecords(log, wg, client, batches, flags.numWorkers, topicName)
+			httpAddRecords(log, wg, client, batches, flags.numWorkers, flags.topicName)
 			wg.Wait()
-			fmt.Printf("Took %s\n", time.Since(t0))
+			fmt.Printf("Took %s\n\n", time.Since(t0))
 		}
 
+		topic, err := client.GetTopic(flags.topicName)
+		if err != nil {
+			log.Fatalf("retrieving topic: %v", err)
+		}
+		fmt.Printf("Topic '%s' next offset: %d\n", topic.Name, topic.NextOffset)
+
+		bufPool := syncy.NewPool(func() []byte {
+			return make([]byte, 0, 50*sizey.MB)
+		})
 		runs := make([]sebbench.Stats, flags.numRuns)
 		for runID := range flags.numRuns {
 			fmt.Printf("Run %d/%d\n", runID+1, flags.numRuns)
 
-			readRequests := make(chan readRequest, flags.numWorkers*2)
+			readRequests := make(chan readRequest, flags.numRequests)
+			totalRecordsRead := generateReadRequests(readRequests, flags.numRequests, topic.NextOffset-1, flags.numRecordsPerRequest)
 
-			// TODO: make parameters configurable
-			numRequests := 10_000
-			go generateReadRequests(readRequests, numRequests, uint64(numRecords), flags.requestRecords)
-
-			wg := &sync.WaitGroup{}
 			t0 := time.Now()
-			httpGetRecords(log, wg, client, flags.numWorkers, readRequests, topicName, bufPool)
-
-			wg.Wait()
+			totalBytesRead := httpGetRecords(log, client, flags.numWorkers, readRequests, flags.topicName, bufPool)
 			elapsed := time.Since(t0)
 
 			runs[runID] = sebbench.Stats{
 				Elapsed:          elapsed,
-				RecordsPerSecond: float64(flags.requestRecords*numRequests) / elapsed.Seconds(),
-				BatchesPerSecond: float64(numRequests) / elapsed.Seconds(),
-				MbitPerSecond:    float64(flags.requestRecords*numRequests*flags.recordSize*8) / 1024 / 1024 / elapsed.Seconds(),
+				RecordsPerSecond: float64(totalRecordsRead) / elapsed.Seconds(),
+				// BatchesPerSecond: float64(flags.numRequests) / elapsed.Seconds(),
+				MbitPerSecond: float64(totalBytesRead*8) / 1024 / 1024 / elapsed.Seconds(),
 			}
 			fmt.Printf("%s\n\n", runs[runID].String())
 		}
 
 		broker.Stop()
-
-		fmt.Printf("Config:\n")
-		fmt.Printf("Num workers:\t\t%d\n", flags.numWorkers)
-		fmt.Printf("Num batches:\t\t%d\n", flags.numBatches)
-		fmt.Printf("Num records/batch:\t%d\n", flags.numRecordsPerBatch)
-		fmt.Printf("Record size:\t\t%s (%dB)\n", sizey.FormatBytes(flags.recordSize), flags.recordSize)
-		fmt.Printf("Total bytes:\t\t%s (%dB)\n", sizey.FormatBytes(totalBytes), totalBytes)
-
-		if flags.localBroker {
-			fmt.Printf("Batch block time:\t%s\n", flags.batchBlockTime)
-			fmt.Printf("Batch bytes max:\t%s (%d)\n", sizey.FormatBytes(flags.batchBytesMax), flags.batchBytesMax)
-		}
-		fmt.Printf("\n")
 
 		sebbench.PrintStats(runs)
 
@@ -141,15 +143,21 @@ var benchmarkReadCmd = &cobra.Command{
 	},
 }
 
-func generateReadRequests(readRequests chan<- readRequest, numRequests int, numRecords uint64, requestRecords int) {
+func generateReadRequests(readRequests chan<- readRequest, numRequests int, maxOffset uint64, recordsPerRequest int) int {
+	totalRecords := 0
 	for range numRequests {
-		offset := uint64y.RandomN(numRecords)
+		offset := uint64y.RandomN(maxOffset)
+		requestRecords := min(int(maxOffset-offset), recordsPerRequest)
 		readRequests <- readRequest{
 			offset:     offset,
-			maxRecords: min(int(numRecords-offset), requestRecords),
+			maxRecords: requestRecords,
 		}
+
+		totalRecords += requestRecords
 	}
 	close(readRequests)
+
+	return totalRecords
 }
 
 type readRequest struct {
@@ -157,13 +165,16 @@ type readRequest struct {
 	maxRecords int
 }
 
-func httpGetRecords(log logger.Logger, wg *sync.WaitGroup, client *seb.RecordClient, workers int, readRequests <-chan readRequest, topicName string, bufPool *syncy.Pool[[]byte]) {
+func httpGetRecords(log logger.Logger, client *seb.RecordClient, workers int, readRequests <-chan readRequest, topicName string, bufPool *syncy.Pool[[]byte]) int64 {
+	wg := &sync.WaitGroup{}
 	wg.Add(workers)
 
+	var totalBytes atomic.Int64
 	for range workers {
 		go func() {
 			defer wg.Done()
 
+			workerTotalBytes := int64(0)
 			for request := range readRequests {
 				buf := bufPool.Get()
 
@@ -180,33 +191,20 @@ func httpGetRecords(log logger.Logger, wg *sync.WaitGroup, client *seb.RecordCli
 					log.Fatalf("expected %d records, got %d (offset %d)", request.maxRecords, len(records), request.offset)
 				}
 
+				for _, record := range records {
+					workerTotalBytes += int64(len(record))
+				}
+
 				bufPool.Put(buf)
 			}
+
+			totalBytes.Add(workerTotalBytes)
 		}()
 	}
+
+	wg.Wait()
+	return totalBytes.Load()
 }
-
-// TODO: we might want to allow running benchmarks without HTTP layer being involved
-// func brokerAddRecords(log logger.Logger, wg *sync.WaitGroup, broker *sebbroker.Broker, batches <-chan recordBatch, workers int, topicName string) {
-// 	wg.Add(workers)
-
-// 	for range workers {
-// 		go func() {
-// 			defer wg.Done()
-
-// 			for batch := range batches {
-// 				offsets, err := broker.AddRecords(topicName, batch.recordSizes, batch.records)
-// 				if err != nil {
-// 					log.Fatalf("failed to add records: %s", err)
-// 				}
-
-// 				if len(offsets) != len(batch.recordSizes) {
-// 					log.Fatalf("expected %d offsets, got %d", len(batch.recordSizes), len(offsets))
-// 				}
-// 			}
-// 		}()
-// 	}
-// }
 
 type BenchmarkReadFlags struct {
 	logLevel   int
@@ -217,13 +215,14 @@ type BenchmarkReadFlags struct {
 	remoteBrokerAddress string
 	remoteBrokerAPIKey  string
 
-	numBatches         int
-	recordSize         int
-	numRecordsPerBatch int
-	requestRecords     int
+	numBatches           int
+	recordSize           int
+	numRecordsPerBatch   int
+	numRecordsPerRequest int
+	numRequests          int
+
+	topicName string
 
 	batchBlockTime time.Duration
 	batchBytesMax  int
-
-	benchmarkDuration time.Duration
 }
