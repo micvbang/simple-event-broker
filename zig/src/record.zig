13976f96347a3ac4a9ba58d35cbb0d90a9ee4f3b
Michael Bang <mic@vbang.dk>
2026-07-08 14:46:08 +0200

    WIP: rename, restructure

--- zig/src/record.zig
+++ zig/src/record.zig
@@ -18,62 +18,68 @@ const PositionalFileReader = struct {
     }
 };
 
-pub const RecordBatch = struct {
-    header: Header,
-    fileSize: usize,
-    maxSize: usize,
+pub fn Parser(comptime Input: type) type {
+    return struct {
+        const Self = @This();
 
-    pub fn init(input: anytype, gpa: std.mem.Allocator, file_size: usize) !RecordBatch {
-        const header = try Header.parse(input, gpa, file_size);
+        header: Header,
+        fileSize: usize,
+        maxSize: usize,
+        input: Input,
 
-        var rb = RecordBatch{
-            .header = header,
-            .fileSize = file_size,
-            .maxSize = 0,
-        };
+        pub fn init(input: Input, gpa: std.mem.Allocator, file_size: usize) !Self {
+            const header = try Header.parse(input, gpa, file_size);
 
-        var max_size: usize = 0;
-        for (0..header.num_records) |record_id| {
-            max_size = @max(rb.size(@intCast(record_id)), max_size);
-        }
-        rb.maxSize = max_size;
+            var rb = Self{
+                .header = header,
+                .fileSize = file_size,
+                .maxSize = 0,
+                .input = input,
+            };
 
-        return rb;
-    }
+            var max_size: usize = 0;
+            for (0..header.num_records) |record_id| {
+                max_size = @max(rb.sizeOf(@intCast(record_id)), max_size);
+            }
+            rb.maxSize = max_size;
 
-    pub fn deinit(self: RecordBatch) void {
-        self.header.deinit();
-    }
-
-    pub fn size(self: RecordBatch, record_id: u32) usize {
-        const record_offset = self.header.record_index[record_id];
-        const next_record_offset = if (record_id + 1 < self.header.num_records)
-            @as(usize, self.header.record_index[record_id + 1])
-        else
-            self.fileSize - self.header.size();
-
-        return next_record_offset - record_offset;
-    }
-
-    pub fn read(self: RecordBatch, input: anytype, record_id: u32, buf: []u8) !usize {
-        if (record_id >= self.header.num_records) return error.RecordNotFound;
-        if (buf.len < self.size(record_id)) return error.BufferTooSmall;
-
-        const record_offset = self.header.record_index[record_id];
-        const record_size = self.size(record_id);
-
-        const read_size = try input.readAt(buf[0..record_size], self.header.size() + record_offset);
-        if (record_size != read_size) {
-            return error.ShortRead;
+            return rb;
         }
 
-        return read_size;
-    }
-};
+        pub fn deinit(self: Self) void {
+            self.header.deinit();
+        }
+
+        pub fn sizeOf(self: Self, record_id: u32) usize {
+            const record_offset = self.header.record_offsets[record_id];
+            const next_record_offset = if (record_id + 1 < self.header.num_records)
+                @as(usize, self.header.record_offsets[record_id + 1])
+            else
+                self.fileSize - self.header.size();
+
+            return next_record_offset - record_offset;
+        }
+
+        pub fn record(self: Self, record_id: u32, buf: []u8) !usize {
+            if (record_id >= self.header.num_records) return error.RecordNotFound;
+            if (self.sizeOf(record_id) > buf.len) return error.BufferTooSmall;
+
+            const record_offset = self.header.record_offsets[record_id];
+            const record_size = self.sizeOf(record_id);
+
+            const read_size = try self.input.readAt(buf[0..record_size], self.header.size() + record_offset);
+            if (record_size != read_size) {
+                return error.ShortRead;
+            }
+
+            return read_size;
+        }
+    };
+}
 
 const Header = struct {
     const header_bytes = 32;
-    const header_index_entry_bytes = 4;
+    const record_offset_size = 4;
 
     // static header
     magic_bytes: [4]u8,
@@ -83,16 +89,16 @@ const Header = struct {
     reserved: [14]u8,
 
     // dynamic header
-    record_index: []u32,
+    record_offsets: []u32,
     allocator: std.mem.Allocator,
 
     const ParseError = error{
         NoRecords,
         EndOfStream,
         FileTooSmall,
-        IndexNotMonotonicallyIncreasing,
-        IndexPointsOutOfBounds,
-        IndexMustStartAtZero,
+        OffsetsNotMonotonicallyIncreasing,
+        OffsetOutOfBounds,
+        OffsetsMustStartAtZero,
     };
 
     fn parse(input: anytype, allocator: std.mem.Allocator, file_size: usize) !Header {
@@ -100,7 +106,7 @@ const Header = struct {
         const read = try input.readAt(&header_buf, 0);
         if (read < header_bytes) return ParseError.EndOfStream;
 
-        // parse static part
+        // parse static fields
         const magic_bytes = header_buf[0..4].*;
         const version = std.mem.readInt(i16, header_buf[4..6], .little);
         const unix_epoch_us = std.mem.readInt(i64, header_buf[6..14], .little);
@@ -109,24 +115,40 @@ const Header = struct {
 
         if (num_records == 0) return ParseError.NoRecords;
 
-        // parse dynamic part (record index)
-        const index_size = @as(usize, num_records) * header_index_entry_bytes;
-        const header_end_offset = header_bytes + index_size;
+        // parse record offsets
+        const record_offsets_size = @as(usize, num_records) * record_offset_size;
+        const header_end_offset = header_bytes + record_offsets_size;
         const file_size_min = header_end_offset + @as(usize, num_records);
         if (file_size < file_size_min) return ParseError.FileTooSmall; // assumes at least 1 byte per record
 
-        var index_buf = try allocator.alloc(u8, index_size);
-        defer allocator.free(index_buf);
+        var record_offsets_buf = try allocator.alloc(u8, record_offsets_size);
+        defer allocator.free(record_offsets_buf);
 
-        const index_read_size = try input.readAt(index_buf, header_bytes);
-        if (index_read_size < index_size) return ParseError.EndOfStream;
+        const read_size = try input.readAt(record_offsets_buf, header_bytes);
+        if (read_size < record_offsets_size) return ParseError.EndOfStream;
 
-        var record_index: []u32 = try allocator.alloc(u32, num_records);
+        var record_offsets: []u32 = try allocator.alloc(u32, num_records);
         for (0..num_records) |i| {
-            record_index[i] = std.mem.readInt(u32, index_buf[i * 4 ..][0..4], .little);
+            record_offsets[i] = std.mem.readInt(u32, record_offsets_buf[i * 4 ..][0..4], .little);
         }
 
-        try Header.validateRecordIndex(record_index, file_size - header_end_offset);
+        // validate record_offsets
+        {
+            // offset must start at 0
+            if (record_offsets[0] != 0) return ParseError.OffsetsMustStartAtZero;
+
+            const record_offsets_max_size = file_size - header_end_offset;
+
+            var previous_offset: u32 = undefined;
+            for (0.., record_offsets) |i, offset| {
+                // Validate that offset doesn't point beyond file length
+                if (offset >= record_offsets_max_size) return ParseError.OffsetOutOfBounds;
+
+                // Verify that offset is monotonically increasing
+                if (i > 0 and previous_offset >= offset) return ParseError.OffsetsNotMonotonicallyIncreasing;
+                previous_offset = offset;
+            }
+        }
 
         return .{
             .magic_bytes = magic_bytes,
@@ -134,32 +156,17 @@ const Header = struct {
             .unix_epoch_us = unix_epoch_us,
             .num_records = num_records,
             .reserved = reserved,
-            .record_index = record_index,
+            .record_offsets = record_offsets,
             .allocator = allocator,
         };
     }
 
-    fn validateRecordIndex(record_index: []u32, index_max: usize) !void {
-        // Index must start at 0
-        if (record_index[0] != 0) return ParseError.IndexMustStartAtZero;
-
-        var last_index: u32 = undefined;
-        for (0.., record_index) |i, index| {
-            // Validate that index doesn't point beyond file length
-            if (index >= index_max) return ParseError.IndexPointsOutOfBounds;
-
-            // Verify that index is monotonically increasing
-            if (i > 0 and last_index >= index) return ParseError.IndexNotMonotonicallyIncreasing;
-            last_index = index;
-        }
-    }
-
     fn deinit(self: Header) void {
-        self.allocator.free(self.record_index);
+        self.allocator.free(self.record_offsets);
     }
 
     fn size(self: Header) usize {
-        return header_bytes + self.num_records * header_index_entry_bytes;
+        return header_bytes + self.num_records * record_offset_size;
     }
 
     pub fn format(self: Header, writer: *std.Io.Writer) !void {
@@ -197,14 +204,14 @@ pub fn main(init: std.process.Init) !void {
     defer f.close();
 
     const file_length = try f.length();
-    const records = try RecordBatch.init(f, init.gpa, file_length);
-    defer records.deinit();
+    const parser = try Parser(@TypeOf(f)).init(f, init.gpa, file_length);
+    defer parser.deinit();
 
-    const buf = try init.gpa.alloc(u8, records.maxSize);
+    const buf = try init.gpa.alloc(u8, parser.maxSize);
     defer init.gpa.free(buf);
 
-    for (0..records.header.num_records) |i| {
-        const size = try records.read(f, @intCast(i), buf[0..]);
+    for (0..parser.header.num_records) |i| {
+        const size = try parser.record(@intCast(i), buf[0..]);
         std.debug.print("{d}: record: {s}\n", .{ i, buf[0..@min(80, size)] });
     }
 }
